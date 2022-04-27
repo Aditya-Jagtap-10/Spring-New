1e77986aaa3e0510ef4df267e0bfc433a7edcfb3
Aditya-Jagtap-10 <102784393+Aditya-Jagtap-10@users.noreply.github.com>
2022-04-27 10:45:17 +0530

    Added new Spring codes
    Running 100%. Not included Spring intro i-access, web transaction & transaction management.

---
+++ Spring Ebox/Intro to spring boot/i assess/DoctorDAo.java
@@ -0,0 +1,28 @@
+package com.springboot.dao;
+
+import java.util.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import com.springboot.domain.Doctor;
+
+@Component
+public class DoctorDAO {
+
+	static ArrayList<Doctor> doctorList = new ArrayList<Doctor>();
+	  static {
+		doctorList.add(new Doctor(1,"Harinii","MBBS",4.2,"Orthologist",750.00));
+		doctorList.add(new Doctor(2,"Nithin","MBBS",2.0,"Gynecologist",1500.00));
+    		doctorList.add(new Doctor(3,"charlotte","MBBS",3.1,"Pediatrics",200.00));
+    		doctorList.add(new Doctor(4,"Lucas","BDS",1.9,"Dentist",250.00));
+	  }	
+
+	public Boolean save(Doctor doctor) {
+		doctorList.add(doctor);
+		return true;
+	}
+	public List<Doctor> list() {
+		return doctorList;
+	}
+	
+}
