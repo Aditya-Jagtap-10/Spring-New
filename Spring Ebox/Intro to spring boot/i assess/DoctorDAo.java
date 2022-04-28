3290878359b139a3ef732ac58f33126263c9a751
Aditya-Jagtap-10 <102784393+Aditya-Jagtap-10@users.noreply.github.com>
2022-04-28 14:20:15 +0530

    Update DoctorDAo.java

--- Spring Ebox/Intro to spring boot/i assess/DoctorDAo.java
+++ Spring Ebox/Intro to spring boot/i assess/DoctorDAo.java
@@ -1,28 +1,25 @@
 package com.springboot.dao;
 
 import java.util.*;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
 import com.springboot.domain.Doctor;
 
 @Component
 public class DoctorDAO {
-
-	static ArrayList<Doctor> doctorList = new ArrayList<Doctor>();
-	  static {
-		doctorList.add(new Doctor(1,"Harinii","MBBS",4.2,"Orthologist",750.00));
-		doctorList.add(new Doctor(2,"Nithin","MBBS",2.0,"Gynecologist",1500.00));
-    		doctorList.add(new Doctor(3,"charlotte","MBBS",3.1,"Pediatrics",200.00));
-    		doctorList.add(new Doctor(4,"Lucas","BDS",1.9,"Dentist",250.00));
-	  }	
-
-	public Boolean save(Doctor doctor) {
-		doctorList.add(doctor);
-		return true;
-	}
+	
+	
+	static List<Doctor> doctorlist = new ArrayList<>(); 
+		static {
+				doctorlist.add(new Doctor(1,"Elizabeth","MBBS",4.2,"Cardiologist",750.00));
+				doctorlist.add(new Doctor(2,"Michael","MBBS",2.0,"Dermatologist",1500.00));
+				doctorlist.add(new Doctor(3,"Charlotte","MBBS",3.1,"Pediatrics",200.00));
+				doctorlist.add(new Doctor(4,"Lucas","BDS",1.9,"Dentist",250.00));
+		}
 	public List<Doctor> list() {
-		return doctorList;
+		//Fill your code here
+		return doctorlist;
+		
 	}
 	
 }
