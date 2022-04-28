2c413f1a3e88df513690c9b125d0eb09f3352a20
Aditya-Jagtap-10 <102784393+Aditya-Jagtap-10@users.noreply.github.com>
2022-04-28 14:20:44 +0530

    Update app.java

--- Spring Ebox/Intro to spring boot/i assess/app.java
+++ Spring Ebox/Intro to spring boot/i assess/app.java
@@ -1,15 +1,13 @@
 package com.springboot;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.springboot.controller.DoctorController;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
-    // fill the code
 		SpringApplication.run(App.class, args);
 	}
 
